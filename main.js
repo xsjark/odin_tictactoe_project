cf4c239ce0b71e8cb6eb14fb966aebdb6a29dd48
Ridha Watanabe <xsjark@protonmail.com>
2022-08-23 11:34:58 -0500

    can only click on empty cells

--- main.js
+++ main.js
@@ -4,39 +4,39 @@ const gameBoard = [
     ["","",""]
 ];
 
-let player = "x"
+let player = "x";
 
 const toggleString = (obj, string1, string2) => {
     player = obj === string1 ?  string2 : string1;
-    console.log(player)
  }
 
 const add = (x, y, symbol, array) => {
-    array[y][x] = symbol;
-    resetUI();
+    if(!array[y][x]) {
+        array[y][x] = symbol;
+        toggleString(player, "x", "o")
+        resetUI();
+    }
 }
 
 const resetUI = () => {
     container.innerHTML = "";
-    renderBoard();
+    renderArray(gameBoard)
 }
 
 const container = document.getElementById("container"); 
 
-const renderBoard = () => {
-    gameBoard.forEach((row, rowno) => {
+const renderArray = (array) => {
+    array.forEach((row, rowno) => {
         row.forEach((cell, cellno) => {
             let cellDiv = document.createElement("div");
             cellDiv.classList.add("cell-div");
             cellDiv.textContent = cell;
             cellDiv.addEventListener("click", () => {
                 add(cellno, rowno, player, gameBoard);
-                toggleString(player, "x", "o")
             });
             container.appendChild(cellDiv)
         })
     })
 }
 
-renderBoard()
-console.log(gameBoard)
+renderArray(gameBoard);
