f06849a8b7ce04d20b05d7018bfa0dceef7bd8ee
Ridha Watanabe <xsjark@protonmail.com>
2022-08-23 13:38:26 -0500

    cleanup

--- main.js
+++ main.js
@@ -9,91 +9,89 @@ let player = "x";
 let winner = "";
 
 const resetArray = () => {
-    gameBoard = [
-        ["", "", ""],
-        ["", "", ""],
-        ["", "", ""],
-    ];
-    console.log(`${player} wins`)
-}
+  gameBoard = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+  console.log(`${player} wins`);
+};
 const checkHorizontal = (row, column, symbol, array) => {
   switch (column) {
     case 0:
-        if (array[row][1] == symbol && array[row][2] == symbol) {
-            return true
-        } else {
-            return false
-        }
+      if (array[row][1] == symbol && array[row][2] == symbol) {
+        return true;
+      } else {
+        return false;
+      }
     case 1:
-        if (array[row][0] == symbol && array[row][2] == symbol) {
-            return true
-        } else {
-            return false
-        }
+      if (array[row][0] == symbol && array[row][2] == symbol) {
+        return true;
+      } else {
+        return false;
+      }
     case 2:
-        if (array[row][0] == symbol && array[row][1] == symbol) {
-            return true
-        } else {
-            return false
-        }
+      if (array[row][0] == symbol && array[row][1] == symbol) {
+        return true;
+      } else {
+        return false;
+      }
     default:
-        return false
+      return false;
   }
 };
 
 const checkVertical = (row, column, symbol, array) => {
-    switch (row) {
-      case 0:
-          if (array[1][column] == symbol && array[2][column] == symbol) {
-            return true
-          } else {
-            return false
-        }
-      case 1:
-          if (array[0][column] == symbol && array[2][column] == symbol) {
-            return true
-          } else {
-            return false
-        }
-      case 2:
-          if (array[0][column] == symbol && array[1][column] == symbol) {
-            return true
-          } else {
-            return false
-        }
-      default:
-          break
-    }
+  switch (row) {
+    case 0:
+      if (array[1][column] == symbol && array[2][column] == symbol) {
+        return true;
+      } else {
+        return false;
+      }
+    case 1:
+      if (array[0][column] == symbol && array[2][column] == symbol) {
+        return true;
+      } else {
+        return false;
+      }
+    case 2:
+      if (array[0][column] == symbol && array[1][column] == symbol) {
+        return true;
+      } else {
+        return false;
+      }
+    default:
+      break;
+  }
 };
 const checkDiagonal = (row, column, symbol, array) => {
-    switch (column) {
-        case 0:
-            if (row == 0 && array[1][1] == symbol && array[2][2]  == symbol) {
-                return true
-            } 
-            else if (row == 2 && array[1][1] == symbol && array[0][2]  == symbol) {
-                return true
-            }  else {
-                return false
-            }
-        case 1:
-            if (row == 1 && array[0][0] == symbol && array[2][2] == symbol) {
-                return true
-            }  else {
-                return false
-            }
-        case 2:
-            if (row == 0 && array[1][1] == symbol && array[2][0]  == symbol) {
-                return true
-            } 
-            else if (row == 2 && array[1][1] == symbol && array[0][0]  == symbol) {
-                return true
-            }  else {
-                return false
-            }
-        default:
-            break
+  switch (column) {
+    case 0:
+      if (row == 0 && array[1][1] == symbol && array[2][2] == symbol) {
+        return true;
+      } else if (row == 2 && array[1][1] == symbol && array[0][2] == symbol) {
+        return true;
+      } else {
+        return false;
       }
+    case 1:
+      if (row == 1 && array[0][0] == symbol && array[2][2] == symbol) {
+        return true;
+      } else {
+        return false;
+      }
+    case 2:
+      if (row == 0 && array[1][1] == symbol && array[2][0] == symbol) {
+        return true;
+      } else if (row == 2 && array[1][1] == symbol && array[0][0] == symbol) {
+        return true;
+      } else {
+        return false;
+      }
+    default:
+      break;
+  }
 };
 
 const toggleString = (obj, string1, string2) => {
