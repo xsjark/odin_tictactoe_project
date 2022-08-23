68582cca3d0c5bad9eeef2e4152942784162cd39
Ridha Watanabe <xsjark@protonmail.com>
2022-08-23 11:28:52 -0500

    works

--- main.js
+++ main.js
@@ -1,22 +1,38 @@
 const gameBoard = [
-    [0,0,0],
-    [0,0,0],
-    [0,0,0]
+    ["","",""],
+    ["","",""],
+    ["","",""]
 ];
+
+let player = "x"
+
+const toggleString = (obj, string1, string2) => {
+    player = obj === string1 ?  string2 : string1;
+    console.log(player)
+ }
 
 const add = (x, y, symbol, array) => {
     array[y][x] = symbol;
-    console.log(gameBoard)
+    resetUI();
+}
+
+const resetUI = () => {
+    container.innerHTML = "";
+    renderBoard();
 }
 
 const container = document.getElementById("container"); 
 
 const renderBoard = () => {
-    gameBoard.forEach(row => {
-        row.forEach(cell => {
+    gameBoard.forEach((row, rowno) => {
+        row.forEach((cell, cellno) => {
             let cellDiv = document.createElement("div");
             cellDiv.classList.add("cell-div");
             cellDiv.textContent = cell;
+            cellDiv.addEventListener("click", () => {
+                add(cellno, rowno, player, gameBoard);
+                toggleString(player, "x", "o")
+            });
             container.appendChild(cellDiv)
         })
     })
